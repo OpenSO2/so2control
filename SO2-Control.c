233392aa7d92a337b04ca14aca8fd40f70832119
Johann Jacobsohn <johann.jacobsohn@zmaw.de>
2014-08-23 15:35:28 +0200

    cleanup

--- SO2-Control.c
+++ SO2-Control.c
@@ -10,14 +10,13 @@
  * s = Structurs (struct)
  */
 
-
 int main( int argc, char* argv[] )
 {
 	/* definition of basic variables */
-	sParameterStruct	sSO2Parameters;
-	flagStruct			sControlFlags;
-	int					state;
-//	etParamValue		eParamValue;
+	sParameterStruct sSO2Parameters;
+	flagStruct       sControlFlags;
+	int              state;
+//	etParamValue     eParamValue;
 
 	/* print welcome message in terminal */
 	printOpening();
@@ -28,7 +27,6 @@ int main( int argc, char* argv[] )
 	memset( &sSO2Parameters, 0, sizeof(sParameterStruct ));
 
 	//function for initialising basic values for sParameterStruct
-
 	state = configurationFunktion(&sSO2Parameters,&sControlFlags);
 	if (state != 0)
 	{
@@ -44,11 +42,14 @@ int main( int argc, char* argv[] )
 		logError("Aquisition failed");
 		return 1;
 	}
+
 	/* Now cease all captures */
 	if ( sSO2Parameters.hCamera ) PHX_Acquire( sSO2Parameters.hCamera, PHX_ABORT, NULL );
 
 	/* Release the Phoenix board */
 	if ( sSO2Parameters.hCamera ) PHX_CameraRelease( &sSO2Parameters.hCamera );
+
 	logExit();
+
 	return 0;
 }
