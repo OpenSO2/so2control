146f21bbd30a36d7b687628fa09ba804157e48b8
Johann Jacobsohn <johann.jacobsohn@zmaw.de>
2015-06-25 20:12:47 +0200

    drop libgen dependency

--- src/io/io-cli.c
+++ src/io/io-cli.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <libgen.h>
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
 #include "getBufferFromFile.c"
@@ -13,23 +12,20 @@
 /* prototypes */
 void parse_filename_to_timeStruct(char * infile, timeStruct *time);
 
-void parse_filename_to_timeStruct(char * infile, timeStruct *time)
+void parse_filename_to_timeStruct(char * inf, timeStruct *time)
 {
-	char *inf = basename(infile);
+	int offset = strlen(inf) - 43;
 	// infile is of the form testing_2014_09_22-23_43_55_984_cam_bot.raw
+	//                       0         1         2         3         4
 	//                       0123456789012345678901234567890123456789012
-	// date is of the form YYYY-MM-DDThh:mm:ss.sssZ (ISO 8601)
-	//                     012345678901234567890123
-	time->year = (inf[ 8] - '0') * 1000 + (inf[ 9] - '0') * 100 + (inf[10] - '0') * 10 + inf[11] - '0';
-	time->mon  = (inf[13] - '0') * 10 + inf[14] - '0';
-	time->day  = (inf[16] - '0') * 10 + inf[17] - '0';
-	time->hour = (inf[19] - '0') * 10 + inf[20] - '0';
-	time->min  = (inf[22] - '0') * 10 + inf[23] - '0';
-	time->sec  = (inf[25] - '0') * 10 + inf[26] - '0';
-	time->milli= (inf[28] - '0') * 100 + (inf[29] - '0') * 10 + inf[30] - '0';
+	time->year = (inf[offset +  8] - '0') * 1000 + (inf[offset +  9] - '0') * 100 + (inf[offset + 10] - '0') * 10 + inf[offset + 11] - '0';
+	time->mon  = (inf[offset + 13] - '0') * 10 + inf[offset + 14] - '0';
+	time->day  = (inf[offset + 16] - '0') * 10 + inf[offset + 17] - '0';
+	time->hour = (inf[offset + 19] - '0') * 10 + inf[offset + 20] - '0';
+	time->min  = (inf[offset + 22] - '0') * 10 + inf[offset + 23] - '0';
+	time->sec  = (inf[offset + 25] - '0') * 10 + inf[offset + 26] - '0';
+	time->milli= (inf[offset + 28] - '0') * 100 + (inf[offset + 29] - '0') * 10 + inf[offset + 30] - '0';
 }
-
-
 
 int main(int argc, char *argv[])
 {
