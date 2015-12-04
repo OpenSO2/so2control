470a564bd21054dfa7ddc4c795d73d3ca5787334
Johann Jacobsohn <johann.jacobsohn@zmaw.de>
2015-12-04 09:38:55 +0100

    add spectrometer mock implementation

---
+++ src/spectrometer/mock/spectrometer.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../spectrometer.h"
+
+int spectrometer_init(void){
+	return 0;
+}
+int spectrometer_get(double * wavelengths, double * spectra, int length)
+{
+	return 0;
+}
+int spectrometer_uninit(void)
+{
+	return 0;
+}
