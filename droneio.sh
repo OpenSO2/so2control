bf0645c49a3225ca0d92748ea51579b4b7e699a1
Johann Jacobsohn <johann.jacobsohn@zmaw.de>
2016-02-08 10:16:11 -0500

    update appveyor and droneio scripts

--- droneio.sh
+++ droneio.sh
@@ -4,6 +4,7 @@ set -u
 set -x
 sudo apt-get update
 sudo apt-get install cmake libopencv-dev libcv-dev libhighgui-dev pngcheck valgrind
+git submodule update --init
 mkdir build
 cd build
 cmake .. -DMOCK_LOG=ON -DMOCK_CAMERA=ON -DMOCK_FILTERWHEEL=ON
