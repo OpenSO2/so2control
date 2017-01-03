39b8286eb45d58a642a8f857baa4416ab8529686
Johann Jacobsohn <johann.jacobsohn@uni-hamburg.de>
2017-01-03 16:47:35 +0100

    make spectrometer liveview more responsive

--- src/spectrometer/spectrometerlive.py
+++ src/spectrometer/spectrometerlive.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import subprocess
+import signal
+import sys
 
 plt.ion()
 fig = plt.figure()
@@ -7,8 +9,26 @@ ax = fig.add_subplot(111)
 plt.show()
 
 first = True
+
+def run():
+	return subprocess.Popen(["./spectrometer-cli", "250"], stdout=subprocess.PIPE)
+
+proc = run()
+
+def signal_handler(signal, frame):
+	proc.kill()
+	proc.wait()
+	sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
 while 1:
-	spectro = subprocess.check_output(["./spectrometer-cli", "250"])
+	retcode = proc.poll()
+	if retcode is None:
+		plt.pause(.01)
+		continue
+
+	spectro = proc.communicate()[0]
 	y = []
 	x = []
 	for line in spectro.split("\n"):
@@ -19,9 +39,12 @@ while 1:
 
 	if first:
 		first = False
-
 		p = ax.plot(x, y, 'r-')[0]
 
 	p.set_ydata(y)
 	plt.draw()
 	fig.canvas.flush_events()
+
+	proc.wait()
+
+	proc = run()
